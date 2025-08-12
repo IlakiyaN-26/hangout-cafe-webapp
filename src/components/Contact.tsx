9325b36b821f5d66314714b916160bacdd368935
Ilakiya N <ilakzilakzilakz@gmail.com>
2025-08-12 17:00:42 +0530

    updated number

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact: React.FC = () => {
   return (
     <section>
       <h2>Contact Us ☕</h2>
-      <p>📞 Phone: +91 6381816307</p>
+      <p>📞 Phone: +91 123456789</p>
       <p>📧 Email: hangoutcafe@example.com</p>
       <p>📍 Address: Thaliyamagathevar theru , Vadasery, Nagercoil</p>
     </section>
